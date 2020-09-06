dd7e97d8b0c15f7cda5fef81c4ad497e6a8c9df0
Hayk Poghosyan <poghossyan.hayk@gmail.com>
2020-09-07 02:45:51 +0400

    ....

---
+++ ToReadApp/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using ToReadApp.Models;
+using ToReadApp.Services;
+
+namespace ToReadApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<ToReadModel> _ToReadBookList;
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\BookList.json";
+        private FileIOService _fileIOService;
+        public MainWindow() 
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _ToReadBookList = _fileIOService.LoadData();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            dgToReadList.ItemsSource = _ToReadBookList;
+            _ToReadBookList.ListChanged += ToReadBookList_ListChanged;
+        }
+
+        private void ToReadBookList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                    Close();
+
+                }
+                dgToReadList.ItemsSource = _ToReadBookList;
+                _ToReadBookList.ListChanged += ToReadBookList_ListChanged;
+            }
+
+        }
+
+
+    }
+}
---
+++ ToReadApp/Services/FileIOService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.IO;
+using ToReadApp.Models;
+
+namespace ToReadApp.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<ToReadModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToReadModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToReadModel>>(fileText);
+            }
+        }
+        public void SaveData(object _ToReadBookList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(_ToReadBookList);
+                writer.Write(output);
+            }
+        }
+    }
+}
